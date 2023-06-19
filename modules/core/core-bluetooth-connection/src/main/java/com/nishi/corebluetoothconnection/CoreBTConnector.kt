b4daa5d6f79724bc082931112583586917c17b20
Bruce Nishimura <bruceninga@gmail.com>
2023-06-19 18:11:46 -0400

    New module for core bluetooth connection. Re-organize BOM for compose/Kotlin

---
+++ modules/core/core-bluetooth-connection/src/main/java/com/nishi/corebluetoothconnection/CoreBTConnector.kt
@@ -0,0 +1,4 @@
+package com.nishi.corebluetoothconnection
+
+class CoreBTConnector {
+}
---
+++ modules/core/core-bluetooth-connection/src/main/java/com/nishi/corebluetoothconnection/CoreBTService.kt
@@ -0,0 +1,10 @@
+package com.nishi.corebluetoothconnection
+
+import android.content.Context
+
+class CoreBTService {
+    companion object {
+        fun startService(context: Context) {
+        }
+    }
+}
